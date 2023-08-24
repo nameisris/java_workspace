a9afac7a68adbf1a2571509968832cc9a30910eb
nameisris <insoo5110@gmail.com>
2023-08-24 22:42:13 +0900

    230824-1

--- 0824/src/ex7/Ex22.java
+++ 0824/src/ex7/Ex22.java
@@ -7,89 +7,96 @@ public class Ex22 {
 	}
 
 }
-//
+
 //abstract class Shape {
-//	Point p;
-//	
-//	Shape() {
-//		this(new Point(0, 0));
-//	}
-//	
-//	Shape(Point p) {
-//		this.p = p;
-//	}
-//	
-//	abstract double calcArea(); // 도형 면적 계산 메서드
-//	
-//	Point getPosition() {
-//		return p;
-//	}
-//	
-//	void setPosition(Point p) {
-//		this.p = p;
-//	}
+//Point p;
+//
+//Shape() {
+//	this(new Point(0, 0));
+//}
+//
+//Shape(Point p) {
+//	this.p = p;
+//}
+//
+//abstract double calcArea(); // 도형 면적 계산 메서드
+//
+//Point getPosition() {
+//	return p;
+//}
+//
+//void setPosition(Point p) {
+//	this.p = p;
+//}
 //}
 //
 //class Point {
-//	int x;
-//	int y;
-//	
-//	Point() {
-//		this(0, 0);
-//	}
-//	
-//	Point(int x, int y) {
-//		this.x = x;
-//		this.y = y;
-//	}
-//	
-//	public String toString() {
-//		return "[" + x + ", " + y + "]";
-//	}
+//int x;
+//int y;
+//
+//Point() {
+//	this(0, 0);
+//}
+//
+//Point(int x, int y) {
+//	this.x = x;
+//	this.y = y;
+//}
+//
+//public String toString() {
+//	return "[" + x + ", " + y + "]";
+//}
 //}
 //
 //class Circle extends Shape {
-//	double r; // 반지름
-//	
-//	Circle() {
-//		super();
-//	}
-//	
-//	Circle(Point p, double r) {
-//		super(p);
-//		this.r = r;
-//	}
-//	
-//	double calcArea() {
-//		
-//		return r * r * Math.PI;
-//	}
+//double r; // 반지름
+//
+//Circle() {
+//	super();
+//}
+//
+//Circle(double r) {
+//	this.r = r;
+//}
+//
+//Circle(Point p, double r) {
+//	super(p);
+//	this.r = r;
+//}
+//
+//double calcArea() {
+//	return r * r * Math.PI;
+//}
 //}
 //
 //class Rectangle extends Shape {
-//	int width; // 폭
-//	int height; // 넓이
-//	
-//	Rectangle() {
-//		super();
+//int width; // 폭
+//int height; // 넓이
+//
+//Rectangle() {
+//	super();
+//}
+//
+//Rectangle(int width, int height) {
+//	this.width = width;
+//	this.height = height;
+//}
+//
+//Rectangle(Point p, int width, int height) {
+//	super(p);
+//	this.width = width;
+//	this.height = height;
+//}
+//
+//double calcArea() {
+//	return width * height;
+//}
+//
+//boolean isSquare() {
+//	if(width == height && (width * height) != 0) {
+//		return true;
 //	}
 //	
-//	Rectangle(Point p, int width, int height) {
-//		super(p);
-//		this.width = width;
-//		this.height = height;
-//	}
-//	
-//	double calcArea() {
-//		
-//		return width * height;
-//	}
-//	
-//	boolean isSquare() {
-//		if(width == height && (width * height) != 0) {
-//			return true;
-//		}
-//		
-//		return false;
-//	}
+//	return false;
+//}
 //}
--- 0824/src/ex7/Ex5.java
+++ 0824/src/ex7/Ex5.java
@@ -13,20 +13,20 @@
 //	int price;			// 제품의 가격
 //	int bonusPoint;		// 제품 구매 시 제공하는 보너스 점수
 //	
-////	Product() {
-////		
-////	}
+//	Product() {
+//		
+//	}
 //	
 //	Product(int price) {
 //		this.price = price;
-//		bonusPoint = (int) (price/10.0);
+//		bonusPoint = (int) (price / 10.0);
 //	}
 //}
 //
 //class Tv extends Product {
-//	
+//
 //	Tv() {
-//		super(1000000);
+//		super(100000);
 //	}
 //	
 //	public String toString() {
