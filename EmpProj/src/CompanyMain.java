3e384491be1793d7d164244b2768c5dbd783c30a
nameisris <68523711+nameisris@users.noreply.github.com>
2023-08-28 09:21:30 +0900

    230828

--- EmpProj/src/CompanyMain.java
+++ EmpProj/src/CompanyMain.java
@@ -19,8 +19,7 @@ public class CompanyMain {
 		
 //		com.regBusinessTrip(emp1, 1); // error, Permanent 출장 불가능
 		com.regBusinessTrip(emp2, 2); // Sales 출장 가능, 하루당 300000원을 incentive로 지급
-		com.regBusinessTrip(emp3, 1
-				); // PartTime 출장 가능, (출장 시간 * payPerime)원을 incentive로 지급 (출장시간은 1일당 24시간)
+		com.regBusinessTrip(emp3, 1); // PartTime 출장 가능, (출장 시간 * payPerime)원을 incentive로 지급 (출장시간은 1일당 24시간)
 		
 		com.allEmployeeInfo();
 		
